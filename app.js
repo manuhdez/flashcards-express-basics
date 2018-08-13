cbb40c062238fd9b7d486adbe008e93d42289b18
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-13 19:17:08 +0100

    Creating a Route with Express

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+})
+
 app.listen(3000);
+console.log('Listening on port 3000...');
