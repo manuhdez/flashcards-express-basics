7785ea5f978981c534f63387a6af63c9fdda0be2
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-13 19:45:38 +0100

    Using Pug in Your Express App

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('<h1>Im running!</h1>');
+  res.render('index');
 });
 
 app.get('/hello', (req, res) => {
