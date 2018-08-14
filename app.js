e98c86cc61d6c8a114ba125ac44f0df683e3a8f1
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-14 12:36:20 +0100

    POST Requests

--- app.js
+++ app.js
@@ -12,13 +12,25 @@ const colors = [
 
 app.set('view engine', 'pug');
 
+// home route
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// cards route
 app.get('/cards', (req, res) => {
   res.render('cards', { propmt: 'Who is buried in grants tomb?', colors});
 });
+
+// hello route
+app.get('/hello', (req, res) => {
+  res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+  res.render('hello');
+});
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000...');
