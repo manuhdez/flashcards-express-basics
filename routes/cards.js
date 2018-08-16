792a0aee30ed8ffc5c9cf8f47783c3dc3fd5c678
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-16 13:05:43 +0100

    Adding Links

--- routes/cards.js
+++ routes/cards.js
@@ -9,9 +9,18 @@ router.get('/:id', (req, res) => {
   const { id } = req.params;
   const text = cards[id][side];
   let { hint } = cards[id];
-  if (side === 'answer') hint = null;
+  let link = {};
 
-  const templateData = { text, hint };
+  if (side === 'answer') {
+    hint = null;
+    link.text = 'Show question';
+    link.url = `/cards/${id}?side=question`;
+  } else {
+    link.text = 'Show answer';
+    link.url = `/cards/${id}?side=answer`;
+  }
+
+  const templateData = { text, hint, link };
 
   res.render('cards', templateData);
 });
