ec80076d65f4320e55cee79f57349916ddbca215
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-16 12:44:05 +0100

    Using Data and Route Parameters

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
 // Cards Routes
-router.get('/', (req, res) => {
-  res.render('cards', { propmt: 'Who is buried in grants tomb?'});
+router.get('/:id', (req, res) => {
+  const cardId = req.params.id;
+  res.render('cards', {
+    prompt: cards[cardId].question,
+    hint: cards[cardId].hint
+  });
 });
 
 module.exports = router;
