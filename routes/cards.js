098301731e5a648b4215ed57cd26d841ae00d836
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-20 13:07:10 +0100

    installed axios for api requests

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
