4fa187d0e00390915c0f1d929319ac85a302f3cf
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-21 11:52:05 +0100

    fixed firebase issue

--- routes/cards.js
+++ routes/cards.js
@@ -1,59 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const firebase = require('firebase');
+const firebase = require('../private/firebaseinit');
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAMXFB2-f91eOJ7TcCmJb56foCr6IEc8i0",
-  authDomain: "flashcards-node.firebaseapp.com",
-  databaseURL: "https://flashcards-node.firebaseio.com",
-  projectId: "flashcards-node",
-  storageBucket: "flashcards-node.appspot.com",
-  messagingSenderId: "816221392665"
-};
-firebase.initializeApp(config);
-
-// Database
-const database = firebase.database();
-const { data } = require('../data/flashcardData.json');
-const { cards } = data;
-
-
+let cardsData = [];
+// Get cards from database
+const cardsDataPromise = new Promise( (resolve, reject) => {
+  resolve(firebase.database().ref('/data/cards').once('value').then( snapshot => snapshot.val()));
+});
 
 // Cards Routes
 router.get('/', (req, res) => {
-  let cardsData = [];
-  database.ref('/data/cards').once('value').then( snapshot => cardsData.push(snapshot.val()));
-  const randomCard = Math.floor(Math.random() * cardsData.length);
-  res.redirect(`/cards/${randomCard}?side=question`);
+  cardsDataPromise.then( (value) => {
+    // store card objects inside cardsData array
+    if (cardsData.length == 0) {
+      Object.keys(value).forEach( val => cardsData.push(value[val]));
+    }
+
+    const randomCard = Math.floor(Math.random() * cardsData.length);
+    // redirect to /:id to render the card
+    res.redirect(`/cards/${randomCard}?side=question`);
+  });
 });
 
 router.get('/:id', (req, res) => {
-  const username = req.cookies.username;
-  const { side } = req.query;
-  const { id } = req.params;
-  const text = cards[id][side];
-  let { hint } = cards[id];
-  let link = {};
 
-  if (!side) {
-    return res.redirect(`/cards/${id}?side=question`);
+  // check if there are new questions on the database
+  cardsDataPromise.then( value => {
+    let valueExtracted = [];
+    Object.keys(value).forEach( val => valueExtracted.push(value[val]));
+    if (valueExtracted.length > cardsData.length) {
+      // store the data with the new questions included
+      cardsData = [];
+      Object.keys(value).forEach( val => cardsData.push(value[val]));
+      sendResponse();
+    } else {
+      sendResponse();
+    }
+  });
+
+  function sendResponse() {
+    // grab request data
+    const username = req.cookies.username;
+    const { side } = req.query;
+    const { id } = req.params;
+    // Initialize card variables
+    let text = cardsData[id][side];
+    let hint = cardsData[id]['hint'];
+    let link = {};
+
+    if (side === 'answer') {
+      hint = null;
+      link.text = 'Show question';
+      link.url = `/cards/${id}?side=question`;
+    } else if (side === 'question'){
+      link.text = 'Show answer';
+      link.url = `/cards/${id}?side=answer`;
+    } else {
+      return res.redirect(`/cards/${id}?side=question`);
+    }
+
+    const templateData = { text, hint, link, username, side };
+    return res.render('cards', templateData);
   }
-
-  if (side === 'answer') {
-    hint = null;
-    link.text = 'Show question';
-    link.url = `/cards/${id}?side=question`;
-  } else if (side === 'question'){
-    link.text = 'Show answer';
-    link.url = `/cards/${id}?side=answer`;
-  } else {
-
-  }
-
-  const templateData = { text, hint, link, username, side };
-
-  res.render('cards', templateData);
 });
 
 module.exports = router;
